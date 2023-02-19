e35aea71815b8aebf5b8562d3656ba71c563a0ba
EvaMicich <6366@holbertonstudents.com>
2023-02-19 02:18:52 -0800

    working on 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -3,14 +3,16 @@
  *main-prints sizes of different types
  *Return: Always 0 (Success)
  */
-int main(void)
+int main (void)
 {
 	int i;
 	double d;
 	char c;
 
-	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
+	printf("Size of a char: %lu.", (unsigned long)sizeof(i) "byte(s)\n");
 	printf("The size of an double is: %lu.\n", (unsigned long)sizeof(d));
+	printf("The size of an c is: %lu.\n", (unsigned long)sizeof(c));
+	printf("The size of an c is: %lu.\n", (unsigned long)sizeof(c));
 	printf("The size of an c is: %lu.\n", (unsigned long)sizeof(c));
 
 	return(0);
