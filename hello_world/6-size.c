78a2bdd38dd1bc2920124a7ce93ea0e782f948ab
EvaMicich <6366@holbertonstudents.com>
2023-02-19 02:43:19 -0800

    fix file 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,7 +11,7 @@ int main(void)
 	long long int  d;
 	float f;
 
-	printf("Size of a char: %lu.\n", (unsigned long)sizeof(c));
+	printf("Size of a char: %lu.\n", (unsigned long)sizeof(c), "byte(s)");
 	printf("Size of an int: %lu.\n", (unsigned long)sizeof(i));
 	printf("Size of a long int: %lu.\n", (unsigned long)sizeof(l));
 	printf("Size of a long long int: %lu.\n", (unsigned long)sizeof(d));
