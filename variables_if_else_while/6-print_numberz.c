8e1436266cc71118345f9083409173413c42e541
EvaMicich <6366@holbertonstudents.com>
2023-02-20 02:15:07 -0800

    added text to file 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		putchar(x);
+		x++;
+	}
+
+	printf("\n");
+
+	return (0);
+
+}
