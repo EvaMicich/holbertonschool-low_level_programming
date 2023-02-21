fa88fee27b073d5e77485140bac4052631c32b7f
EvaMicich <6366@holbertonstudents.com>
2023-02-20 18:45:43 -0800

    added text to file 0 and _putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "_putchar.c"
+
+/**
+ *main - writes the word _putchar
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 0.
+ */
+
+int main()
+{
+	char c = 'a';
+
+	_putchar(c);
+
+	return (0);
+}
