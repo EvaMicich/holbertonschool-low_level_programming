7153a27631179aa4205677442915494a60316aa7
EvaMicich <6366@holbertonstudents.com>
2023-02-20 20:19:44 -0800

    fix code in file 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
-#include "_putchar.c"
-
+#include "main.h"
+#include <stdio.h>
 /**
  *main - writes the word _putchar
  * _putchar - writes the character c to stdout
@@ -8,11 +8,18 @@
  * Return: On success 0.
  */
 
-int main()
+int main(void)
 {
-	char c = 'a';
+	char a[] = "_putchar";
+	int index = 0;
 
-	_putchar(c);
+	while (index < 8)
+	{
+	_putchar(a[index]);
+	index = index + 1;
+	}
+
+	putchar(10);
 
 	return (0);
 }
