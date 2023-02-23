94151731a8ed4f1132defd4d5c970579c6ee7c30
EvaMicich <6366@holbertonstudents.com>
2023-02-23 02:49:08 -0800

    added text to 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds 2 numbers
+ * @a: first number to add
+ * @b: second number to add
+ * Return: the sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
