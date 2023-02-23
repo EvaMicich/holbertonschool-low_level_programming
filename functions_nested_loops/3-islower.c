e2d4a2eea2622d39373f7e52340f02bca37c8b1c
EvaMicich <6366@holbertonstudents.com>
2023-02-22 19:08:41 -0800

    tried to fix betty comment error

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
 #include "main.h"
 /**
-*_islower - checks for lowercase character
-*c is the character being checked for lowercase
-*Return 1 for lower case char, 0 for anything else
-*/
+ * _islower - tests whether a character is a lower case letter
+ * @c: character being checked for lowercase
+ * Return: 1 for lower case letter, 0 for anything else
+ */
 
 int _islower(int c)
 {
