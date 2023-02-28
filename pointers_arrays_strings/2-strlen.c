c53c8c1ef999295669a59bbbe50af67f1dba0003
EvaMicich <6366@holbertonstudents.com>
2023-02-27 23:24:34 -0800

    added text to file 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strlen - returns length of sting s
+* @s: pointer to s string in main
+* return: string length
+*/
+int _strlen(char *s)
+{
+	int n = 0;
+
+	while (*s != '\0')
+	{
+		n = n + 1;
+		s = s + 1;
+	}
+	return (n);
+}
