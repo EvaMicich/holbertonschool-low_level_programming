aa2394c4b3eacefb44b80524bab4d3df4394809c
EvaMicich <6366@holbertonstudents.com>
2023-03-02 02:14:23 -0800

    added text to 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copies string to buffer
+ * @dest: destination buffer
+ * @src: string to be copied, ponter to
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
