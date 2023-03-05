8c51f8baaa9751b5287cf91d18275c7c9990ec59
EvaMicich <6366@holbertonstudents.com>
2023-03-05 04:21:23 -0800

    added correct while loops

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -3,26 +3,29 @@
 
 /**
  * print_diagsums - Prints the sum of the two diagonals
- *                  of a square matrix of integers.
- * @a: The matrix of integers.
- * @size: The size of the matrix.
+ * of a square matrix of integers
+ * @a: The matrix of integers
+ * @size: The size of the matrix of intigers
  */
 void print_diagsums(int *a, int size)
 {
-	int index, sum1 = 0, sum2 = 0;
+	int index = 0, sum1 = 0, sum2 = 0;
 
-	for (index = 0; index < size; index++)
+	while (index < size)
 	{
-		sum1 += a[index];
-		a += size;
+		sum1 = sum1 + a[index];
+		a = a + size;
+		index = index + 1;
 	}
 
-	a -= size;
+	a = a - size;
+	index = 0;
 
-	for (index = 0; index < size; index++)
+	while (index < size)
 	{
-		sum2 += a[index];
-		a -= size;
+		sum2 = sum2 + a[index];
+		a = a - size;
+		index = index + 1;
 	}
 
 	printf("%d, %d\n", sum1, sum2);
