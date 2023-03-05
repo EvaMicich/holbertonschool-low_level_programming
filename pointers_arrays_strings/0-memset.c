9ee1f6af7935c46b10c77795ef6dd5e2b6c8cf1c
EvaMicich <6366@holbertonstudents.com>
2023-03-05 03:17:47 -0800

    review of code

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,14 +1,10 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * memset - Fills the first n bytes of the memory area
- *          pointed to by @s with the constant byte @b.
- * @s: A pointer to the memory area to be filled.
- * @b: The character to fill the memory area with.
- * @n: The number of bytes to be filled.
- *
- * Return: A pointer to the filled memory area @s.
+ *_memset -sets the first n bytes of the memory area pointed to by s to value b
+ *@s: pointer to memory area that will be filled
+ *@n: number of bytes to fill
+ *@b: constant byte
+ *Return: pointer to memory are s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
