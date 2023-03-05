f646a888233044dea637b00f1b40681c40d0637f
EvaMicich <6366@holbertonstudents.com>
2023-03-05 02:57:07 -0800

    fixing the comments

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -31,13 +31,13 @@ char *cap_string(char *str)
 			    str[index - 1] == '{' ||
 			    str[index - 1] == '}' ||
 			    index == 0)
-/*if the remaining lower case is the first char, or preceeded by special char,*/
+/*if the remaining lower case is the first char, or preceeded by special char*/
 			{
 				str[index] = str[index] - 32;
-/*replace with capital letter, by using ascii code - 32. move to the next char*/
-			{
+/*replace with capital letter, using ascii code - 32. move to the next char*/
+			}
 		index = index + 1;
 	}
 	return (str);
-/*return the string with replaced capital letters*/
 }
+/*return the string with replaced capital letters*/
