dbef11e2491943c2e6ddd690559fc06bd5ad0af2
EvaMicich <6366@holbertonstudents.com>
2023-03-05 03:48:59 -0800

    added correct loop

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * _strpbrk - Searches a string for any of a set of bytes.
- * @s: The string to be searched.
- * @accept: The set of bytes to be searched for.
+ * _strpbrk - Searches a string for a set of bytes
+ * @s: The string to be searched
+ * @accept: The bytes to be searched for
  *
- * Return: If a set is matched - a pointer to the matched byte.
- *         If no set is matched - NULL.
+ * Return: If a set is matched - a pointer to the matched byte
+ *         If no match - NULL
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int index;
+	int index = 0;
 
-	while (*s)
+	while (*s != '\0')
 	{
-		for (index = 0; accept[index]; index++)
+		for (index = 0; accept[index]; (index = index + 1))
 		{
 			if (*s == accept[index])
 				return (s);
 		}
 
-		s++;
+		s = s + 1;
 	}
 
 	return ('\0');
