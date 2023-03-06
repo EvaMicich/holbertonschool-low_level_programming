380b93a520ca2b63b6426ab61d0ed982d70d564a
EvaMicich <6366@holbertonstudents.com>
2023-03-06 02:34:32 -0800

    added code to file 4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _pow_recursion - calculates x to the power y
+ * @x: the base number
+ * @y: th epowers number
+ * Return: the vulue of the power x to y, -1 is error
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	else if (y == 0)
+		return (0);
+
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
