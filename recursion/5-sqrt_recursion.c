091ca2f02bf7734949a474fb262ac6a2fa11ce4e
EvaMicich <6366@holbertonstudents.com>
2023-03-06 17:40:20 -0800

    fixed bracket for compiler

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -32,11 +32,17 @@ int _sqr_checker(int a, int n)
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
+	{
 		return (-1);
+	}
 
-	else if ((n == 0) || (n == 1)
+	else if ((n == 0) || (n == 1))
+	{
 		return (n);
+	}
 
 	else if (n > 1)
+	{
 		return (_sqr_checker(1, n));
+	}
 }
