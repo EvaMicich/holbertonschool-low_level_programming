381af03577c043fb9606b0930647947cb07b550d
EvaMicich <6366@holbertonstudents.com>
2023-03-06 17:33:15 -0800

    added code to file 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * _sqr_checker - checks each number from 1 to n to check for square
+ * @n: the number to be tested
+ * @a: square root value being checked
+ * Return: value of natural square root, or -1 for not natural
+ */
+int _sqr_checker(int a, int n)
+{
+	if (a % (n / a) == 0)
+	{
+		if (a * (n / a) == n)
+		{
+			return (a);
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+	else
+	{
+		return (_sqr_checker(a + 1, n));
+	}
+}
+
+/**
+ * _sqrt_recursion - find square root of the number n
+ * @n: the number to be tested
+ * Return: value of natural square root, or -1 for not natural
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	else if ((n == 0) || (n == 1)
+		return (n);
+
+	else if (n > 1)
+		return (_sqr_checker(1, n));
+}
