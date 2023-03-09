0b21e5a81ed9f716bfb6462dd2264643844dd001
EvaMicich <6366@holbertonstudents.com>
2023-03-08 19:58:17 -0800

    added code to file 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *is_num - checks if string is number
+ *@str: string to test
+ *
+ *Return: 1 for number, 0 not number
+ */
+int is_num(char *str)
+{
+	if (*str == '0')
+	{
+		return (1);
+	}
+	else if (atoi(str) == 0)
+	{
+		return (0);
+	}
+	return (0);
+}
+
+/**
+ *main - multiplies 2 numbers
+ *@argc: tesxt from command line
+ *@argv: array referencing text
+ *Return: 0 for success, 1 for failure
+ */
+int main(int argc, char *argv[])
+{
+	int val, sum, i, num_check;
+
+	i = 1;
+	val = 0;
+	sum = 0;
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (i < argc)
+	{
+		num_check = is_num(argv[i]);
+		if (num_check == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			val = atoi(argv[i]);
+			sum = sum + val;
+			i = i + 1;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
