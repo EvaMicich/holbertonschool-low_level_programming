5d202d7011ae853e7899a7823a546827b9bd3432
EvaMicich <6366@holbertonstudents.com>
2023-03-13 03:09:02 -0700

    added null return for null array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -19,6 +19,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	array = malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
 	i = 0;
 	while (i < size)
 	{
