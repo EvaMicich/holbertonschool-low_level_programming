97b704e708a01b4bcce1f883213a1a813ebe8c16
EvaMicich <6366@holbertonstudents.com>
2023-03-13 16:11:39 -0700

    fixed stdlib

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
  * _strdup - returns a pointer to a newly allocated space in memory
  *which contains a copy of the string given as a parameter.
@@ -13,12 +13,10 @@
 char *_strdup(char *str)
 {
 	char *array;
-	int i;
 
 	array = malloc(strlen(str) + 1);
 	if (array == NULL)
 		return (NULL);
-	i = 0;
 	strcpy(array, str);
 	return (array);
 }
