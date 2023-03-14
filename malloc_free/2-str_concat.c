d1830136f77a9a1a3b2b26a37b376a67a2c5d0b0
EvaMicich <6366@holbertonstudents.com>
2023-03-14 03:47:34 -0700

    addde null statements for checker

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,6 +16,14 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	int j;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	array = malloc(sizeof(char) * (strlen(s1) + strlen(s2)) + 1);
 	if (array == NULL)
 		return (NULL);
