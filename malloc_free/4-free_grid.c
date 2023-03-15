fc1690a3c64c336e67f81530ae6eca49b62b3b87
EvaMicich <6366@holbertonstudents.com>
2023-03-14 22:23:37 -0700

    added code to file 4

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -5,13 +5,19 @@
 /**
  * print_grid - prints a grid of integers
  * @grid: the address of the two dimensional grid
- * @width: width of the grid
  * @height: height of the grid
  *
  * Return: Nothing.
  */
 void free_grid(int **grid, int height)
 {
-	grid = 1;
-	height = 1;
+	int i;
+
+	i = 0;
+	while (i < height)
+	{
+		free(grid[i]);
+		i = i + 1;
+	}
+	free(grid);
 }
