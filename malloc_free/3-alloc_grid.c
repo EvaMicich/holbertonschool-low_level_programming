89f63f2c316239e9674c063f4824783f64320097
EvaMicich <6366@holbertonstudents.com>
2023-03-14 21:36:45 -0700

    added code to file 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -4,14 +4,45 @@
 
 /**
  * alloc_grid - returns a pointer to a 2 dimensional array of integers
- * @grid: the address of the two dimensional grid
+ *
  * @width: width of the grid
  * @height: height of the grid
  *
- * Return: Nothing.
+ * Return: pointer to grid
  */
-int **alloc_grid(int width, int height);
+int **alloc_grid(int width, int height)
 {
-	width = 1;
-	height = 1;
+	int i;
+	int j;
+	int **grid;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+	i = 0;
+	while (i < height)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			j = 0;
+			while (j < i)
+			{
+				free(grid[j]);
+				j = j + 1;
+			}
+			free(grid);
+			return (NULL);
+		}
+		j = 0;
+		while (j < width)
+		{
+			grid[i][j] = 0;
+			j = j + 1;
+		}
+		i = i + 1;
+	}
+	return (grid);
 }
