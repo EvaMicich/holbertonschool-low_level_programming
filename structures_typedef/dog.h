b98bf3c62fb50e6a8fa090eed6abe78e957d0974
EvaMicich <6366@holbertonstudents.com>
2023-03-19 18:09:57 -0700

    added description to the struc

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - list of dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: list of name dogs and details
+ */
 struct dog
 {
 	char *name;
