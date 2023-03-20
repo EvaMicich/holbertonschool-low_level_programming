858831a4fac4945afc9632f7b5a5131dd2a9a868
EvaMicich <6366@holbertonstudents.com>
2023-03-19 20:46:02 -0700

    added code to file 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * init_dog - adds dog to list
+ * @d: the struct for dog data
+ * @name: name of dog
+ * @age: age of dog as float
+ * @owner: owner of dog
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
