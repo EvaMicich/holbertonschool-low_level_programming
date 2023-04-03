f27d216b3bdd7717b26b557c6c3b125393375c3a
EvaMicich <6366@holbertonstudents.com>
2023-04-03 04:10:19 -0700

    first draft code to add node to start of singlr linked list in file 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node - new node to begining of list_t
+ *
+ * @head: pointer to pointer to head of the linked list
+ * @str: string to add to node
+ * Return: pointer to linked list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *ptr = malloc(sizeof(list_t));
+	ptr->str = strdup(str);
+	ptr->len = strlen(str);
+	ptr->next = NULL;
+
+	ptr->next = *head;
+	*head = ptr;
+	return (*head);
+}
