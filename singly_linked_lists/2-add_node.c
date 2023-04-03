6e3b6a681fdde9be2a29c6463fe0808e3464379f
EvaMicich <6366@holbertonstudents.com>
2023-04-03 13:37:13 -0700

    handled NULL cases to attempt to fix memory leak

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -14,15 +14,17 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *ptr;
 
-	ptr = malloc(sizeof(list_t));
-	ptr->str = strdup(str);
-	ptr->len = strlen(str);
-	ptr->next = NULL;
-
-	ptr->next = *head;
-	*head = ptr;
+	if (head != NULL && str != NULL)
+	{
+		ptr = malloc(sizeof(list_t));
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
+		ptr->str = strdup(str);
+		ptr->len = strlen(str);
+		ptr->next = *head;
+		*head = ptr;
+	}
 	return (*head);
-	free(ptr->next);
-	free (ptr->str);
-	free (ptr);
 }
