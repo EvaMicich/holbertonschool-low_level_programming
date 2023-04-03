0be1340bcc77f7939ebbf22cc0f507e2e5a8a4ea
EvaMicich <6366@holbertonstudents.com>
2023-04-03 15:11:06 -0700

    first push of code to add node at end of linked list. file 3

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node_end - new node to end of list_t
+ *
+ * @head: pointer to pointer to head of the linked list
+ * @str: string to add to node
+ * Return: pointer to linked list
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *ptr;
+	list_t *temp;
+
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+	temp->str = strdup(str);
+	temp->len = strlen(str);
+	temp->next = NULL;
+	if (*head == NULL)
+	{
+		*head = temp;
+	}
+	else
+	ptr = *head;
+	{
+		while (ptr->next != NULL)
+		{
+			ptr = ptr->next;
+		}
+		ptr->next = temp;
+	}
+	return (*head);
+}
