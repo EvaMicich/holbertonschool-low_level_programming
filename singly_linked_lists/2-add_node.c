06eba2275310f4554d5c68ebf7c726d352a84f9f
EvaMicich <6366@holbertonstudents.com>
2023-04-05 15:23:54 -0700

    made a happy path

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -14,18 +14,18 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
-	if (head != NULL && str != NULL)
+	if (head == NULL && str == NULL)
 	{
-		new_node = malloc(sizeof(new_node));
-		if (new_node == NULL)
-		{
-			return (NULL);
-		}
-		new_node->str = strdup(str);
-		new_node->len = strlen(str);
-		new_node->next = *head;
-		*head = new_node;
-		return (*head);
+		return (NULL);
 	}
-	return (NULL);
+	new_node = malloc(sizeof(new_node));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (*head);
 }
