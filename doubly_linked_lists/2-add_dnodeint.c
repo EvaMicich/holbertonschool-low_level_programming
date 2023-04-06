eb8554ecbce0e6bff5aa28ce3e666d2af94f42e6
EvaMicich <6366@holbertonstudents.com>
2023-04-05 21:04:56 -0700

    added code to file 2.c, adding node to start

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_node - new node to begining of list_t
+ *
+ * @head: pointer to pointer to head of the linked list
+ * @str: string to add to node
+ * Return: pointer to linked list
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	new_node = malloc(sizeof(new_node));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (*head);
+}
