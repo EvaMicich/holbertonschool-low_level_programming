1adf1a721729363248efaf90cedc452831461014
EvaMicich <6366@holbertonstudents.com>
2023-04-05 22:38:44 -0700

    fixed comments

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * add_node - new node to begining of list_t
+ * add_dnodeint - new node to begining of list_t
  *
  * @head: pointer to pointer to head of the linked list
- * @str: string to add to node
- * Return: pointer to linked list
+ * @n: int to add to new node
+ * Return: pointer to double linked list
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
