c5f29403c2554a8e023cd79d22682da575e0ac66
EvaMicich <6366@holbertonstudents.com>
2023-04-06 23:39:18 -0700

    added comments

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * is_binary - checks if char is 0 or 1
+ *
+ * @c: the character
+ * Return: 1 for  binary char, 0 for other.
+ */
+
 int is_binary(char c)
 {
 	if (c != '0' && c != '1')
@@ -9,6 +16,13 @@ int is_binary(char c)
 	}
 	return (1);
 }
+
+/**
+ * binary_to_uint - changes binary chars to a number
+ *
+ * @b: string containing chars of a binary number
+ * Return: the number as an unsigned int
+ */
 
 unsigned int binary_to_uint(const char *b)
 {
