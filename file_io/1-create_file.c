3004f68b68185149c51fe40d50d437fc7cdcd1eb
EvaMicich <6366@holbertonstudents.com>
2023-04-11 19:48:17 -0700

    added code to file 1, creating a file from text

---
+++ file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+/**
+ * create_file - function that creates a file from text
+ * @filename: name of file to create
+ * @text_content: content for file
+ *
+ * Return: 1 for success, -1 failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int written;
+	int len;
+	int i;
+
+	len = strlen(text_content);
+	if (filename == NULL || text_content == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	i = 0;
+	while (text_content[i] != 0)
+	{
+		written = write(fd, text_content, len);
+		i = i + 1;
+	}
+	if (written == -1)
+	{
+		return (-1);
+	}
+	return (1);
+	close(fd);
+}
