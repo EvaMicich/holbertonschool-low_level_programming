4c277183ed56d75542d62f264b3ca88f03bfe7f2
EvaMicich <6366@holbertonstudents.com>
2023-04-11 21:01:19 -0700

    removed null check for string from start of function, moved it to the end

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	int written;
 	int i;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 	{
 		return (-1);
 	}
@@ -23,14 +23,17 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	i = 0;
-	while (text_content[i] != 0)
+	if (text_content != NULL)
 	{
-		written = write(fd, &text_content[i], 1);
-		if (written == -1)
+		while (text_content[i] != '\0')
 		{
-			return (-1);
+			written = write(fd, &text_content[i], 1);
+			if (written == -1)
+			{
+				return (-1);
+			}
+			i = i + 1;
 		}
-		i = i + 1;
 	}
 	close(fd);
 	return (1);
