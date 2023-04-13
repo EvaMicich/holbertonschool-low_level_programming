314da77e304f5d930251f2e4e3b4f883cd0e6b80
EvaMicich <6366@holbertonstudents.com>
2023-04-12 23:56:10 -0700

    correct overwrite node issue

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -51,12 +51,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(key, ht->array[index]->key) == 0)
 		{
 			ht->array[index]->value =(char *)value;
+			return (1);
 		}
 	}
-	else
-	{
-		new_node->next = ht->array[index];
-		ht->array[index] = new_node;
-	}
-		return (1);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
 }
