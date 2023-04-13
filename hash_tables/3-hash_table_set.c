d9680bf53f7f43d507d145d8fb0115d3f9597619
EvaMicich <6366@holbertonstudents.com>
2023-04-12 23:44:23 -0700

    fixed duplication of key

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -46,7 +46,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		return (0);
 	}
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-	return (1);
+	if (ht->array[index] != NULL)
+	{
+		if (strcmp(key, ht->array[index]->key) == 0)
+		{
+			ht->array[index]->value =(char *)value;
+		}
+	}
+	else
+	{
+		new_node->next = ht->array[index];
+		ht->array[index] = new_node;
+	}
+		return (1);
 }
