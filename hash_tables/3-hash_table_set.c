298fd458870d9605d2b90c00f2a63ac3f6691d1e
EvaMicich <6366@holbertonstudents.com>
2023-04-12 23:12:16 -0700

    added code to file 3, adding a node to hash tabble

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,52 @@
+#include "hash_tables.h"
+hash_node_t *make_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(*new_node));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+	return (new_node);
+}
+
+/**
+ * hash_table_set - add elements to hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value associeted to the key to add
+ * Return: 0 fail, 1 success
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node;
+
+	if (ht == NULL || strlen(key) == 0 || value == NULL)
+	{
+		return (0);
+	}
+	index = key_index((const unsigned char *)key, ht->size);
+	new_node = make_node(key, value);
+	if (new_node == NULL)
+	{
+		return (0);
+	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
