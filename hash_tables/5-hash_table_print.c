fed73d9f29be9a74a02ee557f4affa057c518a8c
EvaMicich <6366@holbertonstudents.com>
2023-04-13 18:44:15 -0700

    added code to file 5, to print hash table

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ * @ht: the hash table to print
+ *
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+        unsigned long int i;
+        hash_node_t *head;
+	int init;
+
+	if (ht == NULL)
+	{
+		printf("{}");
+	}
+	i = 0;
+	init = 1;
+	printf("{");
+        while (i < ht->size)
+        {
+                head = ht->array[i];
+                while (head != NULL)
+                {
+			if (init != 1)
+			{
+				printf(", ");
+			}
+			init = 0;
+                        printf("'%s' : '%s'", head->key, head->value);
+                        head = head->next;
+                }
+                i = i + 1;
+        }
+	printf("}\n");
+}
