49d81c79055d0db9463cd8d96f8e48ff8ca3588e
EvaMicich <6366@holbertonstudents.com>
2023-04-15 23:20:25 -0700

    corrected order of files being created

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,66 +1,48 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * xread - reads the fd and handles error
- *
- * @fd: file descriptor
- * @buf: buffer
- * @count: bytes to read
- * @name_of_file: file to read from
- * Return: number of bytes read, or 98 for error
- */
-ssize_t xread(int fd, char *buff, size_t count, char* name_of_file)
-{
-	ssize_t bytes_read;
-
-	bytes_read = read(fd, buff, count);
-	if (bytes_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s",
-			name_of_file);
-		exit(98);
-	}
-	return (bytes_read);
-}
-
-/**
-* main - copies file to another file
-* @file_from: file to copy from
-* @fileto: file to copy to
-*
-* Return: 0 on success
+ * main - copies a file
+ * @argc: count of args
+ * @argv: arr of args
+ * Return: count of chars printed
 */
-
 int main(int argc, char *argv[])
 {
-	int fd_from;
+	int fd_in;
+	int fd_out;
+	int read_out;
+	int write_out;
 	char buff[1024];
-	int num_bytes;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to"), exit(97);
 	}
 	if (argv[1] == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s",
-			argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
+	fd_in = open(argv[1], O_RDONLY);
+	if (fd_in == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s",
-			argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	num_bytes = xread(fd_from, buff, 1024, argv[1]);
-	while (num_bytes != 0)
+	fd_out = open(argv[2], O_CREAT | O_EXCL | O_WRONLY, 0664);
+	if (fd_out < 0)
 	{
-		write(STDOUT_FILENO, buff, num_bytes);
-		num_bytes = xread(fd_from, buff, 1024, argv[1]);
+		fd_out = open(argv[2], O_TRUNC | O_WRONLY);
 	}
-	close(fd_from);
-	return (0);
+	if (fd_out == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+	read_out = read(fd_in, buff, 1024);
+	write_out = write(fd_out, buff, read_out);
+	if (write_out == -1)
+		exit(99);
+	close(fd_in);
+	close(fd_out);
+	return (write_out);
 }
