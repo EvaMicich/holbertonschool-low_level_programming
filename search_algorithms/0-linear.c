81b5476f4098d012242924ed6603930ad319e784
EvaMicich <6366@holbertonstudents.com>
2023-05-22 17:20:50 -0700

    added code for linear search alg

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+/**
+ * linear_search - finds first value in array of integers with liear search
+ * @array: arrray to search
+ * @size: size of array
+ * @value: value to find in the array
+ * Return: success, first index of value. failure -1.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+		i = i + 1;
+	}
+	return (-1);
+}
