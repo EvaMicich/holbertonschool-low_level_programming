5fa46d3e54b37cc3b1cd784ed8943daf17950bdf
EvaMicich <6366@holbertonstudents.com>
2023-05-22 17:40:55 -0700

    fixed edge case and end while loop

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -10,8 +10,12 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
+	if (array == NULL)
+	{
+		return (-1);
+	}
 	i = 0;
-	while (i < size - 1)
+	while (i < size)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
